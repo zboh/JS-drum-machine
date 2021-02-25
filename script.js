d5ab131f76e6f2c573bad8828dd6a4fcbaa6ea85
Herman Sabo <szabo.herman@gmail.com>
2021-02-25 20:09:01 +0400

    refactores js code a bit.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-window.addEventListener('keydown', function(e) {
+
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
     if(!audio) {
@@ -7,15 +8,15 @@ window.addEventListener('keydown', function(e) {
     audio.currentTime = 0; //rewind audio time if key hit again and again
     audio.play();
 
-    key.classList.add('playing');
+    key.classList.add('playing');   
+}
 
-    function removetransition(e){
-        if(e.propertyName !== 'transform') return;
-        this.classList.remove('playing');
+function removetransition(e){
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
 
-    }
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removetransition));
+window.addEventListener('keydown', playSound);
 
-    const keys = document.querySelectorAll('.key');
-    keys.forEach(key => key.addEventListener('transitionend', removetransition));
-
-});
